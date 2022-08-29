9d8c20003e8dff45482f8c89a513fab97ab3be64
Администратор <george2066@yandex.ru>
2022-08-29 18:36:26 +0300

    Шестое домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -3,58 +3,59 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        {
+            /*random & ПЕРВЫЙ ТАСК*/
 
-        /*random & ПЕРВЫЙ ТАСК*/
+            Random random = new Random();
+            int mach = 0;
+            int[] day = new int[30];
 
-        Random random = new Random();
-        int mach = 0;
-        int[] day = new int[30];
-
-        for(int i = 0; i <= (day.length - 1); i++) {
-            int num = random.nextInt(100_000) + 100_000;
-            mach = mach + num;
-            day[i] = num;
-        }
-
-        System.out.printf("Сумма трат за месяц составила %s рублей", mach);
-        System.out.println();
-        System.out.println();
-
-
-
-        /*ВТОРОЙ ТАСК*/
-
-        int max = 0;
-        int min = day[0];
-
-        for (int i = 0; i < day.length; i++) {
-            if(day[i] > max) {
-                max = day[i];
+            for (int i = 0; i <= (day.length - 1); i++) {
+                int num = random.nextInt(100_000) + 100_000;
+                mach = mach + num;
+                day[i] = num;
             }
 
-            if(day[i] < min) {
-                min = day[i];
+            System.out.printf("Сумма трат за месяц составила %s рублей", mach);
+            System.out.println();
+            System.out.println();
+
+
+
+            /*ВТОРОЙ ТАСК*/
+
+            int max = 0;
+            int min = day[0];
+
+            for (int i = 0; i < day.length; i++) {
+                if (day[i] > max) {
+                    max = day[i];
+                }
+
+                if (day[i] < min) {
+                    min = day[i];
+                }
             }
-        }
-        System.out.printf("Минимальная сумма за месяц %s рублей. Максимальная сумма за месяц %s рублей.", min, max);
-        System.out.println();
+            System.out.printf("Минимальная сумма за месяц %s рублей. Максимальная сумма за месяц %s рублей.", min, max);
+            System.out.println();
 
 
 
-        /*Т3РЕ3ТИЙ ТАС3К*/
+            /*Т3РЕ3ТИЙ ТАС3К*/
 
-        System.out.printf("Среднее значение трат за месяц состовляет %s рублей.", (mach % (day.length)));
-        System.out.println();
-        System.out.println();
+            System.out.printf("Среднее значение трат за месяц состовляет %s рублей.", (mach % (day.length)));
+            System.out.println();
+            System.out.println();
 
 
 
-        /*ЧЕТВЁРТ4ЫЙ ТАСК*/
+            /*ЧЕТВЁРТ4ЫЙ ТАСК*/
 
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
 
-        for(int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
+            for (int i = reverseFullName.length - 1; i >= 0; i--) {
+                System.out.print(reverseFullName[i]);
+            }
         }
     }
 }
